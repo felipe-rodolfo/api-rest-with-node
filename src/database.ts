5b97efb7c815a5fd018a7f70d2edf24a1fbe7bc4
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-10-27 16:21:44 -0300

    fix: condicional for db client env

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,15 @@
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
+const clientdb =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? {
+        filename: env.DATABASE_URL,
+      }
+    : env.DATABASE_URL
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection:
-    env.DATABASE_CLIENT === 'sqlite'
-      ? {
-          filename: env.DATABASE_URL,
-        }
-      : env.DATABASE_URL,
+  connection: clientdb,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
