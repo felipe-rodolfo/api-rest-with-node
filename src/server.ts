bf8486bc0f686ff64a267aa4d052863218546d04
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-10-26 21:46:38 -0300

    chore: add dotenv for enviromment variables management

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .select('*')
+    .where('amount', 1000)
+  return transaction
 })
 
 app
